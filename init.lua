d7493bdd735e16df8eac65705d63df53517c35a1
HanSoBored <raihanzxzy@gmail.com>
2026-04-18 23:34:39 +0700

    Add lemonade clipboard and update plugins

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim
 require("config.lazy")
+
+-- lemonade clipboard
+vim.g.clipboard = {
+  name = 'lemonade',
+  copy = {
+    ['+'] = 'lemonade copy',
+    ['*'] = 'lemonade copy',
+  },
+  paste = {
+    ['+'] = 'lemonade paste',
+    ['*'] = 'lemonade paste',
+  },
+  cache_enabled = false,
+}
