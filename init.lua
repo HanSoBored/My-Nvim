cdeb390b4fe1cfa8a0303f200fce409f246d673c
HanSoBored <raihanzxzy@gmail.com>
2026-04-19 00:21:03 +0700

    Fix lemonade clipboard to use --host flag for SSH remote

--- init.lua
+++ init.lua
@@ -5,12 +5,12 @@ require("config.lazy")
 vim.g.clipboard = {
   name = 'lemonade',
   copy = {
-    ['+'] = 'lemonade copy',
-    ['*'] = 'lemonade copy',
+    ['+'] = 'lemonade --host ' .. vim.env.LEMONADE_HOST .. ' copy',
+    ['*'] = 'lemonade --host ' .. vim.env.LEMONADE_HOST .. ' copy',
   },
   paste = {
-    ['+'] = 'lemonade paste',
-    ['*'] = 'lemonade paste',
+    ['+'] = 'lemonade --host ' .. vim.env.LEMONADE_HOST .. ' paste',
+    ['*'] = 'lemonade --host ' .. vim.env.LEMONADE_HOST .. ' paste',
   },
   cache_enabled = false,
 }
